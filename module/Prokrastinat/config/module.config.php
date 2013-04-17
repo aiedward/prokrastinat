d71dae5306bb24e52aa4a8b574d4d77d68c311f3
g1smo <gismopwn@gmail.com>
2013-04-17 14:08:07 +0200

    Prvi controller

--- module/Prokrastinat/config/module.config.php
+++ module/Prokrastinat/config/module.config.php
@@ -14,5 +14,45 @@ return array(
                 )
             )
         )
-    )
+    ),
+	'controllers' => array(
+		'invokables' => array(
+			'Prokrastinat\Controller\Index' => 'Prokrastinat\Controller\IndexController',
+		),
+	),
+	'router' => array(
+		'routes' => array(
+			'home' => array(
+				'type' => 'literal',
+				'options' => array(
+					'route' => '/',
+					'defaults' => array(
+						'controller' => 'Prokrastinat\Controller\Index',
+						'action' => 'index',
+					),
+				),
+			),
+			'index' => array(
+				'type' => 'segment',
+				'options' => array(
+					'route' => '/index[/:action]',
+					'constraints' => array(
+						'action' => '[a-zA-Z][a-zA-Z0-9]*'
+					),
+					'defaults' => array(
+						'controller' => 'Prokrastinat\Controller\Index',
+						'action' => 'index',
+					),
+				),
+			),
+		),
+	),
+	'view_manager' => array(
+		'template_map' => array(
+			'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
 );

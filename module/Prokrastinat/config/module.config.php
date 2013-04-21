b69b4621df4e7c23a3db25dd772d1e4f3547d2e0
g1smo <gismopwn@gmail.com>
2013-04-21 21:00:21 +0200

    Še en merge fail

--- module/Prokrastinat/config/module.config.php
+++ module/Prokrastinat/config/module.config.php
@@ -75,14 +75,10 @@ return array(
         'exception_template'       => 'error/index',
 
 		'template_map' => array(
-<<<<<<< HEAD
-			'layout/layout'           => __DIR__ . '/../../../../layout/layout.phtml',
-=======
-			'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-
-			'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
->>>>>>> 8eac3bc4a9ef7a27160afbeb719950ea70d32249
+		'layout/layout'           => __DIR__ . '/../../../../layout/layout.phtml',
+		'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+		'error/404'               => __DIR__ . '/../view/error/404.phtml',
+		'error/index'             => __DIR__ . '/../view/error/index.phtml',
 		),
 		'template_path_stack' => array(
 			__DIR__ . '/../view',

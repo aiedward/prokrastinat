88017eb3fcaf54fcff43bbbca15e126306a70500
g1smo <gismopwn@gmail.com>
2013-04-21 23:44:36 +0200

    Layout in te zadeve

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,30 @@
  */
 
 return array(
-    // ...
+	'service_manager' => array(
+		'factories' => array(
+			'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
+		),
+	),
+	'translator' => array(
+		'locale' => 'en_US',
+		'translation_file_patterns' => array(
+			array(
+				'type'     => 'gettext',
+				'base_dir' => __DIR__ . '/language',
+				'pattern'  => '%s.mo',
+			),
+		),
+	),
+	'view_manager' => array(
+		'template_map' => array(
+			'layout/layouttwb'			=> 'view/layout.phtml',
+			'error/404'				=> 'view/error/404.phtml',
+			'error/index'			=> __DIR__ . '/../../view/error/index.phtml',
+		),
+		'display_not_found_reason'	=> true,
+		'display_exceptions'		=> true,
+		'not_found_template'		=> 'error/404',
+		'exception_template'		=> 'error/index',
+	),
 );
--- module/Prokrastinat/config/module.config.php
+++ module/Prokrastinat/config/module.config.php
@@ -69,17 +69,6 @@ return array(
 		),
 	),
 	'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-
-		'template_map' => array(
-		'layout/layout'           => __DIR__ . '/../../../../layout/layout.phtml',
-		'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-		'error/404'               => __DIR__ . '/../view/error/404.phtml',
-		'error/index'             => __DIR__ . '/../view/error/index.phtml',
-		),
 		'template_path_stack' => array(
 			__DIR__ . '/../view',
 		),

1875d31172dbe7b12f1ce25611edbf4f4e534abb
Matej <matej.lajh@gmail.com>
2013-05-03 17:04:18 +0200

    neke spremembe

--- module/Prokrastinat/src/Prokrastinat/Controller/IndexController.php
+++ module/Prokrastinat/src/Prokrastinat/Controller/IndexController.php
@@ -5,8 +5,8 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends BaseController
 {
-	public function indexAction()
-	{
-		return new ViewModel();
-	}
+    public function indexAction() 
+    {
+        return new ViewModel();
+    }
 }

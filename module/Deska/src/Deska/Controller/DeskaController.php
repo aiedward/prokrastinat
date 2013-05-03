72b34e896674f495dd63682426ca249ef605382f
Matej <matej.lajh@gmail.com>
2013-05-03 12:32:36 +0200

    prikrit gumb dodaj navadnim userjem, urejen pregled

--- module/Deska/src/Deska/Controller/DeskaController.php
+++ module/Deska/src/Deska/Controller/DeskaController.php
@@ -20,7 +20,10 @@ class DeskaController extends BaseController
         $query = $this->getEntityManager()->createQuery("SELECT o FROM Deska\Entity\Oglas o");
         $oglasi = $query->getResult();
 
-        return new ViewModel(array('oglasi' => $oglasi));
+        return new ViewModel(array(
+                'oglasi' => $oglasi,
+                'tip' => $this->auth->getIdentity()->tip,
+            ));
     }
 
     public function dodajAction() 

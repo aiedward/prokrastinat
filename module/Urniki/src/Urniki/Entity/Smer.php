9b7bbcf30c089f38511087eb7da6738114250c64
Matej <matej.lajh@gmail.com>
2013-05-29 18:40:53 +0200

    smer entity

---
+++ module/Urniki/src/Urniki/Entity/Smer.php
@@ -0,0 +1,29 @@
+<?php
+namespace Urniki\Entity;
+
+use Doctrine\ORM\Query\Expr\Base;
+use Doctrine\ORM\Mapping as ORM;
+use Prokrastinat\Entity\BaseEntity;
+use Urniki\Entity\Program;
+
+/** @ORM\Entity */
+class Smer extends BaseEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+    
+    /** @ORM\ManyToOne(targetEntity="Program") */
+    protected $program;
+    
+    /** @ORM\Column(length=100) */
+    protected $ime;
+    
+    /** @ORM\Column(length=10) */
+    protected $koda;
+    
+    /** @ORM\Column(type="integer") */
+    protected $leta;
+}

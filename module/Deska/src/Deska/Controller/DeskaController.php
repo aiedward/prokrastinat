fcd7838b8cc6c810886693344298db7e9d9483f7
Matej <matej.lajh@gmail.com>
2013-11-27 08:12:17 +0100

    dodan phpmd

--- module/Deska/src/Deska/Controller/DeskaController.php
+++ module/Deska/src/Deska/Controller/DeskaController.php
@@ -22,9 +22,7 @@ class DeskaController extends BaseController
         }
         
         $this->deska_repository = $this->em->getRepository('Deska\Entity\Oglas');
-        //$this->kategorija_repository = $this->em->getRepository('Prokrastinat\Entity\Kategorija');
-        // $this->kategorija_repository->update();
-        //$options = $this->kategorija_repository->getKategorijeInArray();
+
         $user = $this->auth->getIdentity();
         $kategorije = ($user) ? $user->kategorije : array();
         $options = array();
@@ -251,3 +249,4 @@ class DeskaController extends BaseController
         
     }
 }
+?>
